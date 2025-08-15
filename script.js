f301c53e150d28c835fa7bf9b0393a87daf0c3e0
bellebelinhadograu <168314797+bellebelinhadograu@users.noreply.github.com>
2025-08-15 15:10:47 +0000

    estou realizando o projeto

---
+++ script.js
@@ -0,0 +1,76 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativa");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+cosnt perguntas =[
+    {
+     enunciado: "Pergunta 1", 
+   alternativas:[
+    "Alternativa 1", 
+    "Alternativa 2"
+]
+   },
+   {
+     enunciado: "Pergunta 2", 
+   alternativas:[
+    "Alternativa 1", 
+    "Alternativa 2"
+]
+    },
+    {
+     enunciado: "Pergunta 3", 
+   alternativas:[
+    "Alternativa 1", 
+    "Alternativa 2"
+]
+    },
+    {
+      enunciado: "Pergunta 4", 
+   alternativas:[
+    "Alternativa 1", 
+    "Alternativa 2"
+]
+    },
+    {
+      enunciado: "Pergunta 5", 
+   alternativas:[
+    "Alternativa 1", 
+    "Alternativa 2"
+]
+    },
+    {
+      enunciado: "Pergunta 6", 
+   alternativas:[
+    "Alternativa 1", 
+    "Alternativa 2"]
+    },
+    {
+      enunciado: "Pergunta 7", 
+   alternativas:[
+    "Alternativa 1", 
+    "Alternativa 2"
+]
+    },
+    {
+      enunciado: "Pergunta 8", 
+   alternativas:[
+    "Alternativa 1", 
+    "Alternativa 2"]
+    },
+    {
+      enunciado: "Pergunta 9", 
+   alternativas:[
+    "Alternativa 1", 
+    "Alternativa 2"
+]
+    },
+    {
+      enunciado: "Pergunta 10", 
+   alternativas:[
+    "Alternativa 1", 
+    "Alternativa 2"
+]
+     }
+];
